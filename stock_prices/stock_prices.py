44cb3772cf2ef7ec349bb8a0114056c3f326778e
Zurglots <bfeole@gmail.com>
2019-10-10 03:02:28 -0400

    Stock prices working

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -15,41 +15,34 @@ How do
 
 
 def find_max_profit(prices):
-    for i in range(0, len(prices) - 1):
-        cur_index = i
-        largest_index = cur_index
-        for k in range(cur_index+1, len(prices)):
-            if prices[k] > prices[largest_index]:
-                largest_index = k
-            for g in range(1, largest_index - 1):
-                smallest_index = g
-                if prices[g] < prices[g+1]:
-                    prices[g] = smallest_index
-                    print(prices[largest_index])
-                    print(prices[smallest_index])
-                    return prices[largest_index] - prices[smallest_index]
-#                     # if smallest_index in range(largest_index - 1):
+    max_profit = prices[1] - prices[0]
+    for i in range(len(prices) - 1):
+        for k in range(i+1, len(prices)):
+            temporary_max_profit = prices[k] - prices[i]
+            if temporary_max_profit > max_profit:
+                max_profit = temporary_max_profit
+    return max_profit
 
 
-def find_max_profit(prices):
-    for i in range(0, len(prices) - 1):
-        cur_index = i
-        smallest_index = cur_index
-        for k in range(cur_index+1, len(prices)):
-            if prices[k] < prices[smallest_index]:
-                smallest_index = k
-            for g in range(smallest_index+1, len(prices) - smallest_index - 1):
-                largest_index = g
-                if prices[g] > prices[g+1]:
-                    prices[g] = largest_index
-                    print(prices[largest_index])
-                    print(prices[smallest_index])
-                    return prices[largest_index] - prices[smallest_index]
-                    # if smallest_index in range(largest_index - 1):
+# Works for ordered lists
+# def find_max_profit(prices):
+#     for i in range(0, len(prices) - 1):
+#         cur_index = i
+#         largest_index = cur_index
+#         for k in range(cur_index+1, len(prices)):
+#             if prices[k] > prices[largest_index]:
+#                 largest_index = k
+#             for g in range(1, largest_index - 1):
+#                 smallest_index = g
+#                 if prices[g] < prices[g+1]:
+#                     prices[g] = smallest_index
+#                     print(prices[largest_index])
+#                     print(prices[smallest_index])
+#                     return prices[largest_index] - prices[smallest_index]
+# #                     # if smallest_index in range(largest_index - 1):
 
 
 # (find_max_profit([10, 7, 5, 8, 11, 9]), 6)
-
 
 if __name__ == '__main__':
     # This is just some code to accept inputs from the command line
